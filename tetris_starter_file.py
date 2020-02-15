18512f41145b37ab4b36849a35ea98791f03a798
jeff007ali <jagharia@gmail.com>
2020-02-15 13:50:38 +0530

    Code to draw window, grid, create grid with piece

--- tetris_starter_file.py
+++ tetris_starter_file.py
@@ -22,7 +22,7 @@ pygame.font.init()
 s_width = 800
 s_height = 700
 play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 20 height per block
+play_height = 600  # meaning 600 // 20 = 30 height per block
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2
